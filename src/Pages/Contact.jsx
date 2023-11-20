e045fbc7bfb26a335577fcdb5901e37b2ea19154
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-20 18:46:32 +0100

    🎨 style(Contact.jsx) : ajouter le composant Footer pour améliorer la mise en page ✨ fonctionnalité(Contact.jsx) : ajouter le composant Footer pour afficher le pied de page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,12 @@
 import Navigation from "../Components/Navigation"
+import Footer from '../Components/Footer'
 
 function Contact() {
     return(
         <>
             <Navigation />
             <h1>Contact</h1>
+            <Footer />
         </>
     )
 }
