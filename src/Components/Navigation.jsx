9b023d8ad678df90fd1f8e857eae93c7f2aaf8f6
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-20 18:45:14 +0100

    🔧 corriger(Navigation.jsx) : importer les composants manquants et corriger les erreurs de syntaxe ✨ fonctionnalité(Navigation.jsx) : ajouter une barre de navigation avec un menu déroulant pour les petits écrans et un avatar

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,16 +1,122 @@
 import { Link } from "react-router-dom"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import photo from '/medias/PDP-Bilal_pro.JPG?url'
+import { Avatar } from "@mui/material";
 
 function Navigation() {
+    const pages = [
+        { name: 'Accueil', link: '/' },
+        { name: 'Compétences', link: '/competences' },
+        { name: 'Portfolio', link: '/portfolio' },
+        { name: 'Contact', link: '/contact' },
+    ]
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        // event.DefaultPrevent()
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
-        <nav>
-            <img src="" alt="LOGO" />
-            <ul>
-                <Link to={"/"}><li>Acceuil</li></Link>
-                <Link to={"/competences"}><li>Compétences</li></Link>
-                <Link to={"/portfolio"}><li>Portfolio</li></Link>
-                <Link to={"/contact"}><li>Contact</li></Link>
-            </ul>
-        </nav>
+        <AppBar position="static" sx={{backgroundColor:'#081F4B'}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                                    <Link to={page.link}><Typography textAlign="center">{page.name}</Typography></Link>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <Box sx={{ justifySelf: "center", display: { xs: 'flex', md: 'none' } }}>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <Link to={page.link} style={{ listStyleType: 'none', textDecoration: 'none', color: "black" }}>
+                                    <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page.name}</Typography>
+                                    </MenuItem>
+                                </Link>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Avatar src={photo} alt="Photo" style={{width:50, height:50}} />
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:'center' }}>
+                        {pages.map((page) => (
+                            <Link to={page.link} style={{textDecoration:'none'}}> 
+                                <Button
+                                    key={page.name}
+                                    onClick={handleCloseNavMenu}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    {page.name}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
