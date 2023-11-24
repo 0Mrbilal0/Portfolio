72cfdec181f8adc7527cbeba330923c06c65a04f
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-24 10:11:13 +0100

    🔧 corriger(Navigation.jsx) : supprimer les importations inutilisées et corriger les erreurs de syntaxe ✨ fonctionnalité(Navigation.jsx) : mettre à jour la structure de la navigation pour utiliser des balises HTML standard et améliorer l'accessibilité

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,29 +1,21 @@
 import { Link } from "react-router-dom"
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+import '../styles/Navigation.css'
 import photo from '/medias/PDP-Bilal_pro.JPG?url'
-import { Avatar } from "@mui/material";
+// { name: 'Accueil', link: '/' },
 
 function Navigation() {
-    const pages = [
-        { name: 'Accueil', link: '/' },
+    const beforePages = [
         { name: 'Compétences', link: '/competences' },
         { name: 'Portfolio', link: '/portfolio' },
+    ]
+    const afterPages = [
+        { name: 'About Me', link: '/aboutme' },
         { name: 'Contact', link: '/contact' },
     ]
     const [anchorElNav, setAnchorElNav] = React.useState(null);
 
     const handleOpenNavMenu = (event) => {
-        // event.DefaultPrevent()
         setAnchorElNav(event.currentTarget);
     };
 
@@ -32,91 +24,23 @@ function Navigation() {
     };
 
     return (
-        <AppBar position="static" sx={{backgroundColor:'#081F4B'}}>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                                    <Link to={page.link}><Typography textAlign="center">{page.name}</Typography></Link>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-
-                    <Box sx={{ justifySelf: "center", display: { xs: 'flex', md: 'none' } }}>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <Link to={page.link} style={{ listStyleType: 'none', textDecoration: 'none', color: "black" }}>
-                                    <MenuItem key={page.name} onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center">{page.name}</Typography>
-                                    </MenuItem>
-                                </Link>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <Avatar src={photo} alt="Photo" style={{width:50, height:50}} />
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:'center' }}>
-                        {pages.map((page) => (
-                            <Link to={page.link} style={{textDecoration:'none'}}> 
-                                <Button
-                                    key={page.name}
-                                    onClick={handleCloseNavMenu}
-                                    sx={{ my: 2, color: 'white', display: 'block' }}
-                                >
-                                    {page.name}
-                                </Button>
-                            </Link>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+        <header>
+            <nav>
+                <ul>
+                    {beforePages.map((page) => (
+                        <Link to={page.link}><li>{page.name}</li></Link>
+                    ))}
+                </ul>
+                <Link to={'/'}><section id="avatar" ><img src={photo} alt="Avatar" /></section></Link>
+                <ul>
+                    {
+                        afterPages.map((page) => (
+                            <Link to={page.link}><li>{page.name}</li></Link>
+                        ))
+                    }
+                </ul>
+            </nav>
+        </header>
     )
 }
 
