29070bba8cb2cd066ea7d539c9a4602887017416
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-25 15:27:02 +0100

    🔧 corriger(Etoiles.jsx) : réorganiser le code pour améliorer la lisibilité et l'alignement

--- src/Components/Etoiles.jsx
+++ src/Components/Etoiles.jsx
@@ -1,66 +1,65 @@
-import StarIcon from '@mui/icons-material/Star';
+import StarIcon from "@mui/icons-material/Star";
 
-function Etoiles(niveau) {
-
-        switch (niveau.niveau) {
-            case 1:
-                return (
-                    <>
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                    </>
-                )
-            case 2:
-                return (
-                    <>
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                    </>
-                )
-            case 3:
-                return (
-                    <>
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                    </>
-                )
-            case 4:
-                return (
-                    <>
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'black' }} />
-                    </>
-                )
-            case 5:
-                return (
-                    <div style={{display: 'inline'}}>
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        <StarIcon sx={{ color: 'gold' }} />
-                        
-                    </div>
-                )
-            default:
-                return (
-                    <>
-                        <p>None</p>
-                    </>
-                )
-        }
+function Etoiles(competence) {
+  switch (competence.niveau) {
+    case 1:
+      return (
+        <div style={{ display: "flex", justifyContent: "center", paddingTop: "1rem" }}>
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "black" }} />
+          <StarIcon sx={{ color: "black" }} />
+          <StarIcon sx={{ color: "black" }} />
+          <StarIcon sx={{ color: "black" }} />
+        </div>
+      );
+    case 2:
+      return (
+        <div style={{ display: "flex", justifyContent: "center", paddingTop: "1rem" }}>
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "black" }} />
+          <StarIcon sx={{ color: "black" }} />
+          <StarIcon sx={{ color: "black" }} />
+        </div>
+      );
+    case 3:
+      return (
+        <div style={{ display: "flex", justifyContent: "center", paddingTop: "1rem" }}>
+          {" "}
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "black" }} />
+          <StarIcon sx={{ color: "black" }} />
+        </div>
+      );
+    case 4:
+      return (
+        <div style={{ display: "flex", justifyContent: "center", paddingTop: "1rem" }}>
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "black" }} />
+        </div>
+      );
+    case 5:
+      return (
+        <div style={{ display: "flex", justifyContent: "center", paddingTop: "1rem" }}>
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+          <StarIcon sx={{ color: "gold" }} />
+        </div>
+      );
+    default:
+      return (
+        <>
+          <p>None</p>
+        </>
+      );
+  }
 }
 
-export default Etoiles
+export default Etoiles;
