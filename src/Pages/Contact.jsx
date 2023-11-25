ee386115aca0f6ba5e2df8db14ebc6d6d1e4a3c1
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-25 20:16:20 +0100

    🔧 corriger(Contact.jsx) : changer les guillemets doubles en guillemets simples pour l'import de Navigation ✨ fonctionnalité(Contact.jsx) : ajouter des styles CSS pour le composant Contact ✨ fonctionnalité(Contact.jsx) : ajouter un formulaire de contact avec des champs pour le nom, le prénom, l'entreprise, l'email et le message ✨ fonctionnalité(Contact.jsx) : ajouter un bouton d'envoi du formulaire

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,14 +1,50 @@
-import Navigation from "../Components/Navigation"
+import Navigation from '../Components/Navigation'
 import Footer from '../Components/Footer'
+import '../styles/Contact.css'
 
 function Contact() {
-    return(
+
+    const champs = ["Nom", "Prénom", "Entreprise", "Email"]
+
+    return (
         <>
             <Navigation />
-            <h1>Contact</h1>
+            <main id='contact'>
+                <form action="">
+                    <section id='names'>
+                        <section>
+                            <label htmlFor="Lastname" id='lastname-label'>Nom</label>
+                            <input type="text" placeholder='Nom' id='lastname' className='input'/>
+                        </section>
+
+                        <section>
+                            <label htmlFor="Firstname" id='firstname-label'>Prenom</label>
+                            <input type="text" placeholder='Prenom' id='firstname' className='input'/>
+                        </section>
+                    </section>
+
+                    <section>
+                        <label htmlFor="Enterprise" id='enterprise-label'>Entreprise</label>
+                        <input type="text" placeholder="Nom de l'Entreprise" id='enterprise' className='input'/>
+                    </section>
+
+                    <section>
+                        <label htmlFor="Email" id='email-label'>Email</label>
+                        <input type="text" placeholder='Email' id='email' className='input'/>
+                    </section>
+
+                    <section>
+                        <label htmlFor="Message" id='message-label'>Message</label>
+                        <textarea name="Message" id="message" className='input' cols="30" rows="10" placeholder='Message'></textarea>
+                    </section>
+
+                    <button id='button'>Envoyer</button>
+                </form>
+            </main>
             <Footer />
         </>
     )
 }
+
 
 export default Contact
