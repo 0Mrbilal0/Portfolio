cd0924b8ca23ebaf0f31e509814082e5c3b6a498
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-25 15:27:22 +0100

    ✨ fonctionnalité(AboutMe.jsx) : ajouter la page "AboutMe" avec la navigation et le pied de page

---
+++ src/Pages/AboutMe.jsx
@@ -0,0 +1,16 @@
+import Footer from "../Components/Footer"
+import Navigation from "../Components/Navigation"
+
+function AboutMe() {
+    return (
+        <>
+            <Navigation />
+            <main>
+                {/* <embed src={'/public/medias/BILAL_ITCHIR.pdf'} width={"100%"} height={"100%"} type={'application/pdf'} /> */}
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default AboutMe
