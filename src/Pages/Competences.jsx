7736f8f4a6f8c029e8c1f132d5b9168c9bb24023
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-25 15:24:25 +0100

    🎨 style(Competences.jsx): réorganiser les imports et les composants ✨ fonctionnalité(Competences.jsx): ajouter des sections pour afficher les compétences par catégorie (Langages, Frameworks, CMS, Softwares) 🐛 corriger(Competences.jsx): corriger la clé unique pour chaque section de compétence

--- src/Pages/Competences.jsx
+++ src/Pages/Competences.jsx
@@ -1,10 +1,77 @@
+import * as React from 'react'
+import { Input } from '@mui/material'
+import { Search } from '@mui/icons-material'
+
+import Footer from "../Components/Footer"
 import Navigation from "../Components/Navigation"
+import Etoiles from '../Components/Etoiles'
+import '../styles/Competences.css'
+import competences from '../assets/Competences.json'
 
 function Competences() {
-    return(
+    return (
         <>
             <Navigation />
-            <h1>Competences</h1>
+            <main id='competences'>
+                
+                <section id='langages'>
+                    <h2>Langages:</h2>
+                    {
+                        competences.Langages.map((competence) => (
+                            <section className="competence-cards" key={competence.name}>
+                                <ul>
+                                    <li>{competence.name}</li>
+                                    <li><Etoiles niveau={competence.niveau} /> </li>
+                                </ul>
+                            </section>
+                        ))
+                    }
+                </section>
+
+                <section id='frameworks'>
+                    <h2>Frameworks:</h2>
+                    {
+                        competences.Frameworks.map((competence) => (
+                            <section className="competence-cards" key={competence.name}>
+                                <ul>
+                                    <li>{competence.name}</li>
+                                    <li><Etoiles niveau={competence.niveau} /> </li>
+                                </ul>
+                            </section>
+                        ))
+                    }
+                </section>
+
+                <section id='cms'>
+                    <h2>CMS:</h2>
+                    {
+                        competences.CMS.map((competence) => (
+                            <section className="competence-cards" key={competence.name}>
+                                <ul>
+                                    <li>{competence.name}</li>
+                                    <li><Etoiles niveau={competence.niveau} /> </li>
+                                </ul>
+                            </section>
+                        ))
+                    }
+                </section>
+
+                <section id='softwares'>
+                    <h2>Softwares:</h2>
+                    {
+                        competences.Softwares.map((competence) => (
+                            <section className="competence-cards" key={competence.name}>
+                                <ul>
+                                    <li>{competence.name}</li>
+                                    <li><Etoiles niveau={competence.niveau} /> </li>
+                                </ul>
+                            </section>
+                        ))
+                    }
+                </section>
+
+            </main>
+            <Footer />
         </>
     )
 }
