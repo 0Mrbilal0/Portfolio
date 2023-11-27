f9f0367de9d1afd5ca28cb7de87c10573b1880f1
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-27 16:39:21 +0100

    🎨 style(Footer.jsx) : mettre en forme le footer avec un arrière-plan, une ombre et un alignement centré 📝 docs(Footer.jsx) : ajouter une liste de remerciements dans le footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,13 @@
 function Footer() {
     return(
-        <footer style={{display: 'flex' }}>
-            <p>Ceci est mon footer</p>
+        <footer style={{display: 'flex', flexDirection: "column", background: 'url(/medias/bg.jpg) no-repeat',backgroundSize: 'cover', boxShadow: '0 0 2px 2px black', padding: 10, alignItems: 'center', textAlign: 'center', height: '100%' }}>
+            <p>Remerciements: </p>
+            <ul>
+                <li>Prépavenir</li>
+                <li>Raphaël</li>
+                <li>Jenson</li>
+                <li>Mr.Toulouse</li>
+            </ul>
         </footer>
     )
 }
