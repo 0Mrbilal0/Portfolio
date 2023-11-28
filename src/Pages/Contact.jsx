4a4ba6a848e87625f530aab48d740e9b114952d0
Mrbilal <bilal_itchir@Outlook.fr>
2023-11-28 19:54:58 +0100

    🔧 corriger(Contact.jsx) : ajouter la référence à la variable de formulaire (form) pour envoyer le formulaire par email ✨ fonctionnalité(Contact.jsx) : ajouter la fonctionnalité d'envoi de courrier électronique en utilisant emailjs avec les identifiants de service, de modèle et de jeton stockés dans les variables d'environnement VITE_SERVICE_ID, VITE_TEMPLATE_ID et VITE_TOKEN respectivement 🔧 corriger(Contact.jsx) : ajouter les attributs name aux champs de formulaire pour correspondre aux noms des champs dans le modèle de courrier électronique 🔧 corriger(Contact.jsx) : corriger l'orthographe de "envoie" en "envoi" dans la console 🔧 corriger(Contact.jsx) : corriger la fermeture de la balise Alert pour inclure un espace après la couleur de fond verte

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,21 +1,32 @@
 import Navigation from '../Components/Navigation'
 import Footer from '../Components/Footer'
 import '../styles/Contact.css'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { Alert, Snackbar } from '@mui/material'
+import emailjs from '@emailjs/browser';
+const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
+const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID
+const TOKEN = import.meta.env.VITE_TOKEN
 
 function Contact() {
     const [state, setState] = useState(false)
+    const form = useRef();
 
-    function sendMail(e) {
-        e.preventDefault()
-        setState(true)
-        console.log('envoie')
-        setTimeout(() => {
-            setState(false)
-            handleClick()
-        }, 2000);
-    }
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, TOKEN)  
+            .then((result) => {
+                setState(true)
+                console.log('envoie:')
+                console.log(result.text);
+                handleClick()
+
+            }, (error) => {
+                console.log(error.text);
+            }).finally(setState(false));
+
+    };
 
     const [open, setOpen] = useState(false);
 
@@ -35,39 +46,39 @@ function Contact() {
         <>
             <Navigation />
             <main id='contact'>
-                <form action="" onSubmit={sendMail}>
+                <form ref={form} onSubmit={sendEmail}>
                     <section id='names'  >
                         <section>
                             <label htmlFor="Lastname" id='lastname-label'>Nom</label>
-                            <input type="text" placeholder='Nom' id='lastname' className='input' required />
+                            <input type="text" placeholder='Nom' id='lastname' className='input' name='to_lastname' required />
                         </section>
 
                         <section>
                             <label htmlFor="Firstname" id='firstname-label'>Prenom</label>
-                            <input type="text" placeholder='Prenom' id='firstname' className='input' required />
+                            <input type="text" placeholder='Prenom' id='firstname' className='input' name='to_firstname' required />
                         </section>
                     </section>
 
                     <section>
                         <label htmlFor="Enterprise" id='enterprise-label'>Entreprise</label>
-                        <input type="text" placeholder="Nom de l'Entreprise" id='enterprise' className='input' required />
+                        <input type="text" placeholder="Nom de l'Entreprise" id='enterprise' className='input' name='to_enterprise' required />
                     </section>
 
                     <section>
                         <label htmlFor="Email" id='email-label'>Email</label>
-                        <input type="email" placeholder='Email' id='email' className='input' required />
+                        <input type="email" placeholder='Email' id='email' className='input' name='to_email' required />
                     </section>
 
                     <section>
                         <label htmlFor="Message" id='message-label'>Message</label>
-                        <textarea name="Message" id="message" className='input' cols="30" rows="10" placeholder='Message' required></textarea>
+                        <textarea name="to_message" id="message" className='input' cols="30" rows="10" placeholder='Message' required></textarea>
                     </section>
 
                     <button id='button' disabled={state}>{state ? 'Envoie ...' : 'Envoyer'}</button>
                 </form>
             </main>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success" sx={{ width: '100%', backgroundColor: 'green'}}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%', backgroundColor: 'green' }}>
                     Le mail a bien été envoyé !
                 </Alert>
             </Snackbar>
